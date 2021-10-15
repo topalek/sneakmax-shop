5e9eb2814a0b4e7ee2099a918317d367ac3bd15e
topalek <v.topalek@google.com>
2021-10-15 23:47:20 +0300

    desctop

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,6 @@
 @@include('./nouislider.min.js')
 @@include('./quiz.js')
+// @@include('./InstagramFeed.min.js')
 
 function testWebP(callback) {
   var webP = new Image();
@@ -83,3 +84,38 @@ const faqItems = document.querySelectorAll('.faq__item')
 faqItems.forEach(faq =>{
   new Accordion(faq)
 })
+
+function initMap() {
+  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
+// (function(){
+//         new InstagramFeed({
+//             'username': 'kino_teatr_sapfir',
+//             'container': document.querySelector(".insta__content"),
+//             'display_profile': false,
+//             'display_biography': false,
+//             'display_gallery': true,
+//             'display_captions': true,
+//             'max_tries': 8,
+//             'callback': (data)=>{
+//               // data = JSON.parse(data)
+//               console.log(typeof data)
+//             },
+//             'styling': false,
+//             'items': 5,
+//             // 'items_per_row': 4,
+//             // 'margin': 1,
+//             'lazy_load': true,
+//             'on_error': console.error
+//         });
+//     })();
